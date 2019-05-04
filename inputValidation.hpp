1c7d8111f75f243bc434de248d44596785a2e7c5
Katie <stuttskl@gmail.com>
2019-05-04 12:32:57 -0700

    Add files via upload

---
+++ inputValidation.hpp
@@ -0,0 +1,27 @@
+
+#ifndef LAB_5_UPDATED_INPUTVALIDATION_HPP
+#define LAB_5_UPDATED_INPUTVALIDATION_HPP
+#include <string>
+
+
+/*********************************************************************
+** Program name: Lab 4
+** Author: Katie Stutts
+** Date: 4/27/2019
+** Description: Header file for input validation functions. Checks
+if user input is a valid integer, and is a valid integer between a
+given range.
+*********************************************************************/
+
+
+
+int returnInt();
+bool isInt (std::string input);
+bool isInRange(int num, int min, int max);
+
+
+
+
+
+
+#endif //LAB_5_UPDATED_INPUTVALIDATION_HPP
