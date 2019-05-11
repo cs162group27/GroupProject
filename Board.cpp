aedabfedd00ebf68d10c1e39ab45f8131dc8bf7a
april_rochelle <48306452+AprilCastaneda@users.noreply.github.com>
2019-05-10 18:28:51 -0700

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,432 @@
+/*******************************************************************************
+ ** Author:       Victoria Fisher
+ ** Date:         5/12/2019
+ ** Description:  This class sets up the Board, initializes as an array of
+ **               pointers, contains extraCredit() function to set up the Board.
+ *******************************************************************************/
+
+#include "Board.hpp"
+#include "Ant.hpp"
+#include "Doodlebug.hpp"
+#include "Critter.hpp"
+#include "menu.hpp"
+#include <iostream>
+#include <stdlib.h>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+/******************************************************************************
+** Description: Constructor with default values.
+******************************************************************************/
+Board::Board(int s, int r, int c, int a, int d)
+{
+    // Set initial values
+    setSteps(s);
+    setRows(r);
+    setCols(c);
+    setAnts(a);
+    setDoodlebugs(d);
+    setAntCount(0);
+    setDoodlebugCount(0);	
+
+    // Initialize board with Critters
+    initialize();
+}	
+
+/******************************************************************************
+** Description: Display Critters on board.
+******************************************************************************/	
+void Board::display()
+{
+	int antC = 0;
+	int doodleC = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (board[i][j] == nullptr)
+            {
+                cout << '.' << " ";
+            }
+
+            else 
+            {
+                cout << board[i][j] -> getAscii() << " ";
+		
+		if(board[i][j]->getAscii() == 'O')
+			antC++;
+		else
+			doodleC++;
+            }
+
+        }
+
+        cout << endl;
+    }
+	// Debugging display. Counts how many ants and doodlebugs are actually
+	// on the board, regardless what count says. Can delete once program
+	// works
+	cout << "ants: " << antC << endl;
+	cout << "doodlebugs: " << doodleC << endl;
+}
+
+
+
+// Sets the number of rows for the Board.
+void Board::setRows(int r)
+{
+    rows = r;
+}
+
+// Gets the number of rows for the Board.
+int Board::getRows()
+{
+    return rows;
+}
+
+// Sets the number of columns for the Board.
+void Board::setCols(int c)
+{
+    cols = c;
+}
+
+// Gets the number of columns for the Board.
+int Board::getCols()
+{
+    return cols;
+}
+
+// Gets number of Ants.
+int Board::getAnts()
+{
+    return ants;
+}
+
+// Sets number of Ants.
+void Board::setAnts(int a)
+{
+    ants = a;
+}
+
+// Gets number of Doodlebugs.
+int Board::getDoodlebugs()
+{
+    return doodlebugs;
+}
+
+// Returns number of Doodlebugs.
+void Board::setDoodlebugs(int d)
+{
+    doodlebugs = d;
+}
+
+
+void Board::setSteps(int s)
+{
+    steps = s;
+}
+
+//// Gets the number of steps to take.
+int Board::getSteps()
+{
+    return steps;
+}
+
+void Board::setAntCount(int aCount)
+{
+    antCount = aCount;
+}
+
+int Board::getAntCount()
+{
+    return antCount;
+}
+
+void Board::setDoodlebugCount(int dCount)
+{
+    doodlebugCount = dCount;
+}
+
+int Board::getDoodlebugCount()
+{
+    return doodlebugCount;
+}
+
+
+
+/* Initializes the Board to empty squares. Places Critters on Board.
+Citation for implementation: https://stackoverflow.com/questions/27430523/2d-array-of-object-pointers-in-c */
+void Board::initialize()
+{
+//    srand(9);		// srand in main to allow seeding with time
+    board = new Critter ** [rows];
+
+    for (int i = 0; i < rows; i++) {
+        board[i] = new Critter *[cols];
+    }
+/*	Not creating a Critter object for every block in board
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = new Critter;
+            }
+        }
+*/
+  for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            board [i][j] = nullptr;
+        }
+    }
+
+//    for (int i = 0; i < ants; i++) {.
+   // Create ants until required ants number reached 
+  while(antCount < ants)
+  {	
+	aRow = rand()%rows;
+        aCol = rand()%cols;
+	
+	// If ant doesn't already exist at location
+	if(board[aRow][aCol] == nullptr)
+        {
+		board[aRow][aCol] = new Ant;
+		antCount++;
+	}
+  }
+  //  }
+
+//    for (int i = 0; i < doodlebugs; i++) {
+   // Create doodleugs until required doodlebugs number reached 
+  while(doodlebugCount < doodlebugs)
+  {
+	dRow = rand()%rows;
+        dCol = rand()%cols;
+        
+	// If doodlebug doesn't already exist at location
+	if(board[dRow][dCol] == nullptr)
+	{
+		board[dRow][dCol] = new Doodlebug;
+        	doodlebugCount++;
+	}
+  }
+  //  }
+}
+
+/******************************************************************************
+** Description: Function that runs board. Displays board and runs Critter 
+**		actions within user inputted time steps.
+******************************************************************************/
+void Board::run()
+{
+    // Display initialized board on Day 1
+    display();
+    cout << "Initialized Board. Day 1 Start." << endl;
+
+    // Run Board. Doodlebugs move, Ant move, Doodlebug breed, Ant breed, 
+    // Doodlebug starve, Increment Critters age, Display resulting board 
+    // end of day. 
+    for (int k = 0; k < steps; k++)
+    {
+	// Reset Critters' moved flag to 0
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < cols; j++)
+			if(board[i][j] != nullptr)
+				board[i][j]->setMoved(0);
+
+	// STEP ONE: Move Critters (Doodlebugs move before Ants, requirement)
+      	// A: Move doodlebugs first
+	for (int i = 0; i < rows; i++)
+        {
+           	for(int j = 0; j < cols; j++)
+            	{
+			// Does the pointer point to a Critter?
+               	 	if(board[i][j] != nullptr)
+                	{
+				// Is the Critter a Doodlebug?
+				if(board[i][j]->getAscii() == 'X')
+				{
+				//	cout << "Are we in move doodlebug?" << endl;
+					// Move Doodlebug
+					board[i][j]->move(board, i, j, rows, cols);
+				}
+			}	
+		}
+	
+	}
+
+      	// B: Move Ants next
+	for (int i = 0; i < rows; i++)
+        {
+           	for(int j = 0; j < cols; j++)
+            	{
+			// Does the pointer point to a Critter?
+               	 	if(board[i][j] != nullptr)
+                	{
+				// Is the Critter an Ant?
+				if(board[i][j]->getAscii() == 'O')
+				{
+				//	cout << "Are we in move Ant?" << endl;
+					// Move Ant
+					board[i][j]->move(board, i, j, rows, cols);
+				}
+			}	
+		}
+	
+	}
+
+	// STEP TWO: Breed Critters (Doodlebugs breed before Ants, for consistency)
+      	// A. Breed Doodlebugs first
+	for (int i = 0; i < rows; i++)
+        {
+           	for(int j = 0; j < cols; j++)
+            	{
+			// Only breed if there is space on the board
+			if((doodlebugCount+antCount) < (rows*cols))
+			{
+				// Does the pointer point to a Critter?
+				if(board[i][j] != nullptr)
+				{
+					// Is the Criter a Doodlebug?
+					if(board[i][j]->getAscii() == 'X')
+					{
+						bool doodlebugBred = 0;
+		
+						// Breed Doodlebug
+						doodlebugBred = board[i][j]->breed(board, i, j, rows, cols);
+
+						// If Doodlebug bred, increment count and reset age
+						if(doodlebugBred)
+						{
+				//			cout << "Did we breed Doodlebug?" << endl;
+							doodlebugCount++;
+						}
+					}
+				}
+			}	
+		}
+	
+	}
+
+	// B. Breed Ants next
+	for (int i = 0; i < rows; i++)
+        {
+           	for(int j = 0; j < cols; j++)
+            	{
+			// Only breed if there is space on the board
+			if((doodlebugCount+antCount) < (rows*cols))
+			{
+				// Does the pointer point to a Critter?
+				if(board[i][j] != nullptr)
+				{
+					// Is the Critter an Ant?
+					if(board[i][j]->getAscii() == 'O')
+					{
+						bool antBred = 0;
+
+						// Breed  Ant
+						antBred = board[i][j]->breed(board, i, j, rows, cols);
+						
+						// If Ant bred, increment count and reset Ant age
+						if(antBred)
+						{
+							antCount++;
+						}
+					}
+				}
+			}	
+		}
+	
+	}
+
+	// STEP 3: Starve
+	// Put starve code here
+
+	// STEP 4: Increment Critters' ages
+	// Increment Critters' ages
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < cols; j++)
+			if(board[i][j] != nullptr)
+				board[i][j]->incrementAge();
+	
+	// STEP 5: Display Board end of day - results of Critter actions.
+	display();
+	cout << "End of day " << k+1 << endl;
+
+     }
+}
+		/*
+                    if (board[i][j] -> getAge() > 3)
+                    {
+                        board[i][j] -> breed(board, i, j, rows, cols);
+                        board[i][j] -> setAge(0);
+                    }
+                    else if (board[i][j] -> getAge() > 8)
+                    {
+                        board[i][j] -> breed(board, i, j, rows, cols);
+                        board[i][j] -> setAge(0);
+                    }
+                    if(board[i][j] == nullptr)
+                    {
+                        occupied = 0;
+                    }
+
+                    else
+                    {
+                        occupied = 1;
+                    }
+
+                    if(i >= rows)
+                    {
+                        offGrid = 1;
+                    }
+
+                    else if(j >= cols)
+                    {
+                        offGrid = 1;
+                    }
+
+                    else if(i < 0)
+                    {
+                        offGrid = 1;
+                    }
+
+                    else if(j < 0)
+                    {
+                        offGrid = 1;
+                    }
+
+                    else
+                    {
+                        offGrid = 0;
+                    }
+
+                    if(offGrid && occupied)
+                    {
+                        board[i][j]->move(board, i, j, rows, cols);
+                        // move should be a function within Critter class, not Board
+                    }
+		
+                    board[i][j]->incrementAge();
+                    // increment age is a function within Critter class, not Board
+                }
+            }
+        }
+
+        display();
+
+        int totalAnts = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                totalAnts += board[i][j]->checkAnt(board, i, j);
+            }
+        }
+
+        cout << totalAnts;
+
+        cout << endl;
+    }
+*/
+
---
+++ Doodlebug.cpp
@@ -0,0 +1,412 @@
+//
+// Created by Patrick Oh on 2019-05-07.
+//
+
+#include <random>
+#include "Doodlebug.hpp"
+#include "Critter.hpp"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Doodlebug::Doodlebug(): Critter(0, 0, 'X')
+{
+    antsEaten = 0;
+}
+
+Doodlebug::Doodlebug(const Doodlebug &obj):Critter(obj.age, obj.moved, obj.ascii)
+{
+    antsEaten = 0;
+}
+/******************************************************************************
+** Description: Function that moves Doodlebuges.
+******************************************************************************/
+bool Doodlebug::move(Critter ***grid, int gridX, int gridY, int numRows, int numCols)
+{
+	// Only move Doodlebug if Doodlebug hasn't already moved this turn
+	if(!moved)
+	{
+		// To generate random number
+		int randNum = 0;
+		std::random_device seed;	// Will be used to obtain a seed for the random
+					// number engine
+		std::mt19937 gen(seed());	// Standard mersenne_twister_engine seeded with seed()
+
+		int bugX = gridX,
+		bugY = gridY,
+		count1 = 0,
+		count2 = 0,
+		count3 = 0,
+		count4 = 0;
+
+		bool full = 0;	// Variable to see if cell is occupied.
+		bool offBounds = 0;	// Variable to see if cell is off grid.
+		bool antFound = 0;
+
+		// Look at all directions around doodlebug and find a space that's not offgrid
+		// and contains an Ant
+		do	
+		{
+			// Check South if not offgrid and there is an Ant
+			if(!isOffGrid(gridX+1, gridY, numRows, numCols) && (count1<1))
+			{
+				if(isAnt(grid, gridX+1, gridY))
+				{
+					bugX = gridX + 1;
+					bugY = gridY;
+					antFound = 1;
+					count1++;
+					break;
+				}
+				else
+					count1++;
+			}
+			// Check East if not offgrid and there is an Ant
+			if(!isOffGrid(gridX, gridY+1, numRows, numCols) && (count2<1))
+			{
+				if(isAnt(grid, gridX, gridY+1))
+				{
+					bugX = gridX;
+					bugY = gridY + 1;
+					antFound = 1;
+					count2++;
+					break;
+				}
+				else
+					count2++;
+			}
+			// Check North if not offgrid and there is an Ant
+			if(!isOffGrid(gridX-1, gridY, numRows, numCols) && (count3<1))
+			{
+				if(isAnt(grid, gridX-1, gridY))
+				{
+					bugX = gridX - 1;
+					bugY = gridY;
+					antFound = 1;
+					count3++;
+					break;
+				}
+				else
+					count3++;
+			}
+			// Check Wast if not offgrid and there is an Ant
+			if(!isOffGrid(gridX, gridY-1, numRows, numCols) && (count4<1))
+			{
+				if(isAnt(grid, gridX, gridY-1))
+				{
+					bugX = gridX;
+					bugY = gridY - 1;
+					antFound = 1;
+					count4++;
+					break;
+				}
+				else
+					count4++;
+			}
+			else	// No Ants here
+			{
+				break;	
+			}
+		} // Loop until all possible directions have been explored
+		while(!antFound && ((count1 < 1) || (count2 < 1) || (count3 < 1) || (count4 < 1)));
+
+		// If no Ants found around Doodlebug, move like an Ant.
+		if(!antFound)	
+		{
+			// Generate random number
+			std::uniform_int_distribution<> dis(1, 4);
+			randNum = dis(gen);	// Assign generated randNum to variable
+
+			// Do not move Doodlebug yet. Assign temporary variables and check
+			// neighboring cells.
+			// If 1 = South
+			if(randNum == 1)
+			{
+			    bugX = gridX + 1;
+			    bugY = gridY;
+			}
+			// If 2 == East
+			else if(randNum == 2)
+			{
+			    bugX = gridX;
+			    bugY = gridY + 1;
+			}
+			// If 3 == North
+			else if(randNum == 3)
+			{
+			    bugX = gridX - 1;
+			    bugY = gridY;
+			}
+			// Else 4 == West
+			else
+			{
+			    bugX = gridX;
+			    bugY = gridY - 1;
+			}
+
+			// Now, see if neighboring cell is offBounds or occupied
+			offBounds = isOffGrid(bugX, bugY, numRows, numCols);
+			bool occupied = 0;	
+
+			// If not offBounds, check if occupied
+			if(!offBounds)
+			{
+				// Is there a Critter in coordinates?
+				occupied = isOccupied(grid, bugX, bugY);
+			
+				// If not offgrid have no adjacent critters, 
+				// then Doodlebug moves.
+				if(!offBounds && !occupied)
+				{
+				    // Store new direction's pointer
+				    Critter *tempPtr = grid[bugX][bugY];
+				
+				    // Swap pointer values
+				    grid[bugX][bugY] = grid[gridX][gridY];
+				    grid[gridX][gridY] = tempPtr;
+			
+				    grid[bugX][bugY]->setMoved(1);
+						
+				    return 1;	
+				}
+				else // Offbounds or occupied, doesn't move
+				{
+				    return 0;
+				}
+			}
+			else // Offbounds, doesn't move
+			{
+				return 0;
+			}
+		}
+		// Else, an Ant has been found at bugX, bugY coordinates
+		else
+		{
+		
+			// Eat Ant
+			eatAnt(grid, bugX, bugY);
+
+			// Move Doodlebugs after having eaten	
+			// Store new direction's pointer
+			Critter *tempPtr = grid[bugX][bugY];
+			
+			// Swap pointer values
+			grid[bugX][bugY] = grid[gridX][gridY];
+			grid[gridX][gridY] = tempPtr;
+			
+			grid[bugX][bugY]->setMoved(1);
+			
+			// Increment Ants eaten
+			grid[bugX][bugY]->incrementAntsEaten();
+			
+						
+			return 1;
+		}
+	}
+	else	// Already moved this turn
+	{
+		return 0;
+	}	
+}
+
+/******************************************************************************
+** Description: Function that breeds Doodlebugs.
+******************************************************************************/
+bool Doodlebug::breed(Critter ***grid, int gridX, int gridY, int numRows, int numCols)
+{
+    // Check if Doodlebug can breed (age >= 8)
+    if(age >= 8)
+    {
+        int randNum = 0,
+            bugX = 0,
+            bugY = 0,
+            count1 = 0, count2 = 0, count3 = 0, count4 = 0;
+
+        bool occupied = 0; // Variable to see if cell is occupied.
+        bool offGrid = 0; // Variable to see if cell is off grid.
+
+
+        std::random_device seed;
+        std::mt19937 gen(seed());
+        std::uniform_int_distribution<> dis(1, 4);
+
+	// Produce random number to determine direction to breed
+        do
+        {
+            randNum = dis(gen);
+            if(randNum == 1)
+            {
+                bugX = gridX + 1;
+                bugY = gridY;
+
+                count1++;
+            }
+
+            else if(randNum == 2)
+            {
+                bugX = gridX - 1;
+                bugY = gridY;
+
+                count2++;
+            }
+
+            else if(randNum == 3)
+            {
+                bugX = gridX;
+                bugY = gridY - 1;
+
+                count3++;
+            }
+
+            else if(randNum == 4)
+            {
+                bugX = gridX;
+                bugY = gridY + 1;
+
+                count4++;
+            }
+
+	    // Now, see if neighboring cell is off grid
+	    offGrid = isOffGrid(bugX, bugY, numRows, numCols);
+
+	    // If not offGrid, check to see if it's occupied
+	    if(!offGrid)
+	    {
+		if(isOccupied(grid, bugX, bugY))
+		{
+			occupied = 1;
+		}
+		else
+		{
+			occupied = 0;
+		}	
+            }
+
+        } // Loop until an available neighboring cell is found or ALL cells unavailable
+	while((occupied || offGrid) && ((count1 < 1) || (count2 < 1) || (count3 < 1) || (count4 < 1)));
+
+        if(!offGrid && !occupied)
+        {
+            grid[bugX][bugY] = new Doodlebug;	// Create new Doodlebug
+	    setAge(0);	// Reset age of Doodlebug
+	    return 1;
+        }
+	else // No Doodlebugs bred
+	{
+	    return 0;
+	}
+    }
+    else // No Doodlebugs old enough
+    {
+	return 0;
+    }	
+}
+
+int Doodlebug::checkAnt(Critter ***grid, int x, int y)
+{
+    if(grid[x][y] -> getAscii() == 'O')
+    {
+        antCount++;
+    }
+
+    return antCount;
+}
+
+void Doodlebug::eatAnt(Critter ***grid, int x, int y)
+{
+    Critter *tempPtr = nullptr;
+    delete grid[x][y];
+    grid[x][y] = tempPtr;
+
+//    antsEaten++;
+}
+
+void Doodlebug::survive(Critter ***grid, int x, int y)
+{
+    if(antsEaten == 0 && age == 3)
+    {
+        grid[x][y] = nullptr;
+    }
+}
+
+/******************************************************************************
+** Description: Function that determines if adjacent space is offgrid.
+******************************************************************************/
+bool Doodlebug::isOffGrid(int x, int y, int numRows, int numCols)
+{
+    // Check if neighboring cell is off the grid.
+    // Moving clockwise
+    // Are the coordinates at the East/Right edge of the grid?
+    if(x >= numRows)
+    {
+	return 1;
+    }
+	// Are the coordinates at the South/Bottom edge of the grid?
+    else if(y >= numCols)
+    {
+	return 1;
+    }
+	// Are the coordinates at the West/Left edge of the grid?
+    else if(x < 0)
+    {
+	return 1;
+    }
+	// Are the coordinates the the North/Top edge of the grid?
+    else if(y < 0)
+    {
+	return 1;
+    }
+	// Coordinates are within limits
+    else
+    {
+	return 0;
+    }
+}
+
+/******************************************************************************
+** Description: Function that determines if adjacent space is an Ant.
+******************************************************************************/
+bool Doodlebug::isAnt(Critter ***grid, int x, int y)
+{
+	// Does the pointer point to a Critter?
+	if(grid[x][y] != nullptr)
+	{
+		// Does the pointer point to an Ant?
+		if(grid[x][y]->getAscii() == 'O')
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else // Pointer points to nullptr
+	{
+		return 0;
+	}	
+}
+
+/******************************************************************************
+** Description: Function that determines if adjacent space has a Critter.
+******************************************************************************/
+bool Doodlebug::isOccupied(Critter ***grid, int x, int y)
+{
+	if(grid[x][y] != nullptr)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/******************************************************************************
+** Description: Increment ants eaten.
+******************************************************************************/
+void Doodlebug::incrementAntsEaten()
+{
+	antsEaten++;
+}
